73d3692adafa37a5ed9279b83f07458040c40134
Glauco Serrano <glaucoserrano@gmail.com>
2021-03-11 12:17:42 -0300

    desafio concluido

--- src/index.js
+++ src/index.js
@@ -22,18 +22,24 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
+  repositories.push(repository);
 
-  return response.json(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
+  
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
+  }
+
+  if(updatedRepository.likes > 0){
+    updatedRepository.likes = 0;
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
@@ -48,7 +54,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +72,9 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  //const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes = ++repositories[repositoryIndex].likes;
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
